402cde57ad134b6cce8eb37ad2aa6fe10cb1492d
jonathano88 <jonathano88@gmail.com>
2024-11-03 21:36:44 +1300

    mixing

--- mixer.py
+++ mixer.py
@@ -1,15 +1,19 @@
 import pandas as pd
-
+path='C:\\Users\\jonat\\OneDrive - Whitecliffe College\\IT9115 Project\\Data\\CSV\\'
 # Read CSV files\
-df_A = pd.read_csv('C:\\Users\\jonat\\OneDrive - Whitecliffe College\\IT9115 Project\\Data\\dataset_20240907_plusP.csv')
-df_B = pd.read_csv('C:\\Users\\jonat\\OneDrive - Whitecliffe College\\IT9115 Project\\Data\\dataset_20241023_plusP.csv')
+df_A = pd.read_csv(path+'dataset_20241023.csv')
+df_B = pd.read_csv(path+'dataset_20241028.csv')
+df_C = pd.read_csv(path+'dataset_20241029.csv')
+
 
 # Set the ID field as the index
 df_A.set_index('id', inplace=True)
 df_B.set_index('id', inplace=True)
+df_C.set_index('id', inplace=True)
 
 # Combine the DataFrames, giving priority to df_B
 merged_df = df_B.combine_first(df_A).reset_index()
+merged_df = df_C.combine_first(merged_df).reset_index()
 
 # Save the merged DataFrame to a new CSV file
-merged_df.to_csv('C:\\Users\\jonat\\OneDrive - Whitecliffe College\\IT9115 Project\\Data\\dataset_merged_plusP.csv', index=False)
+merged_df.to_csv(path+'dataset_merged2.csv', index=False)
